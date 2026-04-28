1cef813a508bd832140d9c580c000dbe103b4e33
geovannadarosa <geovannadarosava@gmail.com>
2026-04-28 16:37:37 -0400

    fazendo atividade e arrumando o html e o css

--- js/7.js
+++ js/7.js
@@ -1,13 +1,30 @@
 function reajuste(){
+    console.log("a")
     let salario = Number(document.getElementById('salario').value);
-    
     if(salario <= 280){
-        document.getElementById('antes-do-reajuste').innerText = salario  (salario + 1,2  ) / salario * 100;
-        document.getElementById('porcentual-aumento').innerText = "20%";
+        document.getElementById('antes-reajuste').innerText = `salario atual ${salario}`
+        document.getElementById('porcentual-aumento').innerText = '20% de aumento'
+        document.getElementById('aumento').innerText = `valor de aumento ${(salario * 1.2) - salario}`
+        document.getElementById('novo-salario').innerText = `salario novo ${salario * 1.2}`
+
 
     }
     else if(salario <= 700){
-        document.gete
+        document.getElementById('antes-reajuste').innerText = `salario atual ${salario}`
+        document.getElementById('porcentual-aumento').innerText = '20% de aumento'
+        document.getElementById('aumento').innerText = `valor de aumento ${(salario * 1.15) - salario}`
+        document.getElementById('novo-salario').innerText = `salario novo ${salario * 1.15}`
     }
-
+    else if(salario <= 1500){
+        document.getElementById('antes-reajuste').innerText = `salario atual ${salario}`
+        document.getElementById('porcentual-aumento').innerText = '20% de aumento'
+        document.getElementById('aumento').innerText = `valor de aumento ${(salario * 1.1) - salario}`
+        document.getElementById('novo-salario').innerText = `salario novo ${salario * 1.1}`
+    }
+    else{
+        document.getElementById('antes-reajuste').innerText = `salario atual ${salario}`
+        document.getElementById('porcentual-aumento').innerText = '20% de aumento'
+        document.getElementById('aumento').innerText = `valor de aumento ${(salario * 1.05) - salario}`
+        document.getElementById('novo-salario').innerText = `salario novo ${salario * 1.05}`
+    }
 }
